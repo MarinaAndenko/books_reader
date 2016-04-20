9a8e54b09e986a1cbba8165679b6954683487872
Marina <marina.andenko@gmail.com>
2016-04-20 16:07:26 +0300

    gmail confirmation devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   ###########################
-  Rails.application.routes.default_url_options[:host] = 'booksreader.herokuapp.com'
-  #config.action_mailer.default_url_options[:host] = 'booksreader.herokuapp.com'
+  config.action_mailer.default_url_options[:host] = 'booksreader.herokuapp.com'
   #config.action_mailer.raise_delivery_errors = true
   #config.action_mailer.perform_deliveries = true
   #config.action_mailer.delivery_method = :smtp
