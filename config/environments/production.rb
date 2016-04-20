dad89c1e896d8493f3022fac7b192dc591b34c27
Marina <marina.andenko@gmail.com>
2016-04-20 19:53:22 +0300

    action_mailer updates2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,8 +36,8 @@ Rails.application.configure do
     :address                  => 'smtp.gmail.com',
     :port                     => 587,
     :authentication           => 'plain',
-    :user_name                => ENV["GMAIL_USERNAME"],
-    :password                 => ENV["GMAIL_PASSWORD"],
+    :user_name                => ENV["booksapplication100500@gmail.com"],
+    :password                 => ENV["books100500"],
     :domain                   => 'heroku.com',
     :enable_starttls_auto     => true
   }  
