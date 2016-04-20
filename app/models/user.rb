9988b56c7f4f18cdfb4c70197d631d12ac62a98f
Marina <marina.andenko@gmail.com>
2016-04-20 21:34:29 +0300

    skip confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  before_save :skip_confirmation!
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, 
          :validatable, :confirmable
